0fd5a9737b0700deb8db3b3da74d1f19f00f2508
kmilogp8496 <kmilogp8496@gmail.com>
2024-09-07 01:18:43 +0200

    removed unnecessary git from lang

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -19,7 +19,6 @@ require("lazy").setup({
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     { import = "lazyvim.plugins.extras.lang.vue" },
-    { import = "lazyvim.plugins.extras.lang.git" },
     { import = "lazyvim.plugins.extras.linting.eslint" },
     -- import/override with your plugins
     { import = "plugins" },
