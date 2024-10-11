9cd28ba2f4780c43656066fc9ec76a764dc79404
camilo.gonzalez <camilo.gonzalez@check24.es>
2024-10-11 12:10:28 +0200

    Added php support

---
+++ lua/plugins/php.lua
@@ -0,0 +1,28 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    optional = true,
+    opts = {
+      formatters_by_ft = {
+        php = { { "pint", "php_cs_fixer" } },
+      },
+    },
+  },
+  {
+    -- Remove phpcs linter.
+    "mfussenegger/nvim-lint",
+    optional = true,
+    opts = {
+      linters_by_ft = {
+        php = {},
+      },
+    },
+  },
+  {
+    -- Add neotest-pest plugin for running PHP tests.
+    -- A package is also available for PHPUnit if needed.
+    "nvim-neotest/neotest",
+    dependencies = { "V13Axel/neotest-pest" },
+    opts = { adapters = { "neotest-pest" } },
+  },
+}
