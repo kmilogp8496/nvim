70c8c100bb535a0113483d494c026c70c01ebe59
Kmilo <kmilogp8496@gmail.com>
2026-02-27 19:31:50 +0100

    Added default options for AI integration

--- lua/custom/plugins/opencode.lua
+++ lua/custom/plugins/opencode.lua
@@ -1,3 +1,16 @@
 return {
   'git@github.com:kmilogp/opencode.nvim',
+  opts = {
+    default_profile = 'fast',
+    profiles = {
+      fast = {
+        provider = 'cursor_agent',
+        model = 'composer-1.5',
+      },
+      deep = {
+        provider = 'cursor_agent',
+        model = 'Opus 4.6',
+      },
+    },
+  },
 }
