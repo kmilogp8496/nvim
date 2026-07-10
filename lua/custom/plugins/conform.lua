991c4e35d69b06eb52be59b71b8eb3158522a50b
Camilo González <camilo.gonzalez@check24.es>
2026-07-10 14:07:58 +0200

    Disable languages formatting

--- lua/custom/plugins/conform.lua
+++ lua/custom/plugins/conform.lua
@@ -4,7 +4,7 @@ if not ok then return end
 conform.setup {
   notify_on_error = false,
   format_on_save = function(bufnr)
-    local disable_filetypes = { c = true, cpp = true }
+    local disable_filetypes = { c = true, cpp = true, vue = true, javascript = true, typescript = true, php = true, json = true, markdown = true }
     local ft = vim.bo[bufnr].filetype
     local bufname = vim.api.nvim_buf_get_name(bufnr)
 
